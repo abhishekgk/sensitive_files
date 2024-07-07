bee40955efd5565355a1f5c09bee4512943c3619
abhishekgk <70231985+abhishekgk@users.noreply.github.com>
2024-07-07 10:20:12 +0530

    Create installation_files.sh

---
+++ installation_files.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install golang-go
+sudo apt install assetfinder
+sudo apt install amass
+sudo apt install theharvester
+sudo apt install httprobe
+sudo apt install gospider
+sudo apt install katana
+sudo apt install ffuf
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/hakluke/hakrawler@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
